9db757ef638db07e12d5f02de6c1a1eb2b598e6b
kennedybailey <121199666+kennedybailey@users.noreply.github.com>
2023-01-07 15:04:45 -0500

    updated default display and date

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
+//local default display city and date
 let displayCity = document.querySelector("#display-city");
+let displayDate = document.querySelector("#display-date");
 let localTime = moment.tz.guess();
-displayCity.innerHTML = localTime;
 
+let cityName = localTime.replace("_", " ").split("/")[1];
+let cityDate = moment().tz(localTime).format("dddd, MMMM Do YYYY");
+
+displayCity.innerHTML = cityName;
+displayDate.innerHTML = cityDate;
+
+//changing country in select
 let selectCountry = document.querySelector("#country");
